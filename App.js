36aac6c0890cec600090e5b411bcaead8c41b7bc
rubyseher <rubyseher2002@gmail.com>
2021-07-13 12:30:31 +0530

    displayData

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Auth,
   createConnection,
@@ -7,12 +7,15 @@ import {
   createLongLivedTokenAuth,
 } from "home-assistant-js-websocket";
 import {
+  ScrollView,
   Text,
   View,
 } from 'react-native';
 import { REACT_APP_ACCESS_TOKEN } from '@env';
 
 const App = () => {
+  const [data, setData] = useState()
+
   useEffect(async () => {
     const auth = createLongLivedTokenAuth(
       "http://home.local:8123",
@@ -21,11 +24,20 @@ const App = () => {
 
     const connection = await createConnection({ auth });
     subscribeEntities(connection, (entities) => {
-      console.log(entities["switch.bedlight"]);
+      setData(entities)
     });
+
   }, [])
   return (
-    <View></View>
+    <ScrollView>
+      {
+        data && Object.keys(data).map((item, i) => (
+          
+          <Text>{item}</Text>
+        ))
+      }
+
+    </ScrollView>
   );
 };
 export default App;
