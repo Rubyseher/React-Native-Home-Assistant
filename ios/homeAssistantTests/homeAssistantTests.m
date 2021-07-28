22400bed068811ae4bf3f08996872faf03d53cf1
rubyseher <rubyseher2002@gmail.com>
2021-07-29 00:24:44 +0530

    rename

--- ios/homeAssistantTests/homeAssistantTests.m
+++ ios/homeAssistantTests/homeAssistantTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface homeAssistantTests : XCTestCase
+@interface homeassistantTests : XCTestCase
 
 @end
 
-@implementation homeAssistantTests
+@implementation homeassistantTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
