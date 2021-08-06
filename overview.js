470a9f14d123463eba15a09214ec1f4b96cae0bc
rubyseher <rubyseher2002@gmail.com>
2021-08-06 17:07:49 +0530

    gradient background

--- overview.js
+++ overview.js
@@ -1,17 +1,20 @@
 import React, { useRef, useContext, useEffect, useState } from 'react';
 import { callService, createConnection, subscribeEntities, createLongLivedTokenAuth, } from "home-assistant-js-websocket";
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import { REACT_APP_ACCESS_TOKEN } from '@env';
 import { styles, colors } from './styles';
 import MIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { AnySizeDragSortableView } from 'react-native-drag-sort';
 import { DataContext } from './App';
+import Svg, { LinearGradient, Defs, Stop, Rect } from 'react-native-svg';
+
 const collection = require("./collection.json")
 
 const Overview = () => {
   const { data, connection } = useContext(DataContext)
   const [layoutItems, setLayoutItems] = useState([])
   const [layoutSizes, setLayoutSizes] = useState(collection)
+  const { height, width } = Dimensions.get('window');
   const sortableViewRef = useRef();
   const contextMenu = [
     { key: 'foo', title: 'Foo' },
@@ -41,36 +44,49 @@ const Overview = () => {
         onPressOut={() => { sortableViewRef.current.onPressOut() }}
       >
         <MIcon name={layoutSizes[item].icon} size={iconSizes[layoutSizes[item].size]} color={(data[item].state === "on" ? colors.white : colors.darkBlue)} solid />
-        {layoutSizes[item].size != "small" ? <Text style={{color:(data[item].state === "on" ? colors.white : colors.darkBlue)}}>{data[item].attributes.friendly_name}</Text> : null}
+        {layoutSizes[item].size != "small" ? <Text style={{ color: (data[item].state === "on" ? colors.white : colors.darkBlue) }}>{data[item].attributes.friendly_name}</Text> : null}
       </TouchableOpacity>
     )
   }
 
   return (
-    <View style={{ backgroundColor: colors.white, height: '100%', width: '100%', paddingHorizontal: 20, paddingTop: 40 }}>
-      <View
+    <View>
+      <Svg height={height} width={width} style={{ zIndex: 0 }}>
+        <Defs>
+          <LinearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
+            <Stop offset="0" stopColor="#FFD080" stopOpacity="1" />
+            <Stop offset="1" stopColor="red" stopOpacity="1" />
+          </LinearGradient>
+        </Defs>
+        <Rect x="25" y="5" width={width} height={height} fill="url(#grad)" />
+      </Svg>
+
+      <View style={{ height: '100%', width: '100%', paddingHorizontal: 20, paddingTop: 40,position:'absolute'}}>
+        {/* <View
         contextMenu={contextMenu}
         onContextMenuItemClick={event => {
           console.log(event.nativeEvent)
         }}>
+      </View> */}
+
         <Text style={styles.title}>Overview</Text>
+        {
+          data && <AnySizeDragSortableView
+            movedWrapStyle={styles.entitiesBox}
+            ref={sortableViewRef}
+            dataSource={layoutItems.filter(function (item) {
+              return (item.split(".")[0] === "scene" || item.split(".")[0] === "switch");
+            })}
+            keyExtractor={(item, i) => i}
+            renderItem={_renderItem}
+            onDataChange={(d, callback) => {
+              console.log(d);
+              setLayoutItems(d);
+              callback();
+            }}
+          />
+        }
       </View>
-      {
-        data && <AnySizeDragSortableView
-          movedWrapStyle={styles.entitiesBox}
-          ref={sortableViewRef}
-          dataSource={layoutItems.filter(function (item) {
-            return (item.split(".")[0] === "scene" || item.split(".")[0] === "switch");
-          })}
-          keyExtractor={(item, i) => i}
-          renderItem={_renderItem}
-          onDataChange={(d, callback) => {
-            console.log(d);
-            setLayoutItems(d);
-            callback();
-          }}
-        />
-      }
     </View>
   )
 }
